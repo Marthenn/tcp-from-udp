42bf7f5b299e70deaa82b744bc1a7b5c25a9cf9f
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-11-28 21:06:38 +0700

    feat (md5sum): make a md5sum checker program

---
+++ md5sum.py
@@ -0,0 +1,21 @@
+import hashlib
+
+import hashlib
+
+def calculate_md5(file_path):
+    with open(file_path, 'rb') as file:
+        data = file.read()
+        md5_hash = hashlib.md5()
+        md5_hash.update(data)
+        return md5_hash.hexdigest()
+
+def compare_files(file_path1, file_path2):
+    return calculate_md5(file_path1) == calculate_md5(file_path2)
+
+if __name__ == '__main__':
+    file1 = input('Enter the path of the first file: ')
+    file2 = input('Enter the path of the second file: ')
+    if compare_files(file1, file2):
+        print('Files are identical')
+    else:
+        print('Files are not identical')
